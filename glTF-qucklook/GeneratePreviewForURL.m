f56b9818b83992f1f4a15c5b7db7ff8dba11a66b
Anton Klochkov <tklochkov@gmail.com>
2018-12-03 19:31:35 +0300

    Code refactoring

--- glTF-qucklook/GeneratePreviewForURL.m
+++ glTF-qucklook/GeneratePreviewForURL.m
@@ -2,12 +2,7 @@
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
 
-#import "GLTFErrorCheckerByJSON.h"
-#import "GLTFErrorCheckerByScenes.h"
-
-@import SceneKit;
-@import GLTF;
-@import GLTFSCN;
+#include "SceneGenerator.h"
 
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
@@ -18,63 +13,6 @@ void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
    This function's job is to create preview for designated file
    ----------------------------------------------------------------------------- */
 
-CFDataRef archivedScene(const SCNScene *scene) {
-    return (__bridge CFDataRef)[NSKeyedArchiver archivedDataWithRootObject:scene requiringSecureCoding:NO error:nil];
-}
-
-void setAnimations(SCNScene *scene, GLTFSCNAsset *scnAsset) {
-    NSDictionary *animations = scnAsset.animations;
-    
-    if (animations.count != 0) {
-        NSString *name = scnAsset.animations.allKeys.firstObject;
-#if DEBUG
-        NSLog(@"Animation name: %@", name);
-#endif
-        
-        [animations[name] enumerateObjectsUsingBlock:^(GLTFSCNAnimationTargetPair *pair, NSUInteger index, BOOL *stop) {
-            pair.animation.usesSceneTimeBase = NO;
-            [pair.target addAnimation:pair.animation forKey:nil];
-        }];
-    }
-}
-
-void setErrorScene(SCNScene **scene) {
-#if DEBUG
-    NSLog(@"Error Scene");
-#endif
-    NSBundle *bundle = [NSBundle bundleWithIdentifier:@"com.ITS.glTF-qucklook"];
-    
-    NSURL *urlFile =  [bundle URLForResource:@"ErrorSymbol" withExtension:@"scn"];
-    
-    *scene = [SCNScene sceneWithURL:urlFile options:nil error:nil];
-}
-
-
-CFDataRef sceneByURL(NSURL* url, QLPreviewRequestRef* preview) {
-    SCNScene *scene = nil;
-    
-    if (![GR isGoodGLTFByName:url.path.UTF8String]) {
-        setErrorScene(&scene);
-        
-        return archivedScene(scene);
-    }
-    
-    
-    id<GLTFBufferAllocator> bufferAllocator = [[GLTFDefaultBufferAllocator alloc] init];
-    GLTFAsset *asset = [[GLTFAsset alloc] initWithURL:url bufferAllocator:bufferAllocator];
-    
-    if (asset == nil || ![GLTFErrorCheckerByScenes isGoodGLTFByScenes:asset.scenes]) {
-        setErrorScene(&scene);
-    } else {
-        GLTFSCNAsset *scnAsset = [SCNScene assetFromGLTFAsset:asset options:@{}];
-        scene = scnAsset.defaultScene;
-        setAnimations(scene, scnAsset);
-    }
-    
-    
-    return archivedScene(scene);
-}
-
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
     // To complete your generator please implement the function GeneratePreviewForURL in GeneratePreviewForURL.c
@@ -84,13 +22,7 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
             return noErr;
         }
         
-        if(QLPreviewRequestIsCancelled(preview)){
-            return noErr;
-        }
-        
-        QLPreviewRequestSetDataRepresentation(preview, sceneByURL((__bridge NSURL*)url, &preview), kUTType3DContent, options);
-        
-        return noErr;
+        QLPreviewRequestSetDataRepresentation(preview, [SceneGenerator sceneDataByURL:url], kUTType3DContent, options);
     }
     
     return noErr;
---
+++ glTF-qucklook/SceneGenerator.m
@@ -0,0 +1,81 @@
+//
+//  SceneGenerator.m
+//  glTF-qucklook
+//
+//  Created by Klochkov Anton on 03/12/2018.
+//  Copyright © 2018 Klochkov Anton. All rights reserved.
+//
+
+#import "SceneGenerator.h"
+
+#import "GLTFErrorCheckerByJSON.h"
+#import "GLTFErrorCheckerByScenes.h"
+
+@import GLTF;
+@import GLTFSCN;
+
+@implementation SceneGenerator
+
++ (CFDataRef)sceneDataByURL:(CFURLRef)url {
+    return [SceneGenerator archivedScene:[SceneGenerator sceneByURL:url]];
+}
+
++ (SCNScene *)sceneByURL:(CFURLRef)url {
+    return [SceneGenerator sceneByNSURL:(__bridge NSURL*)url];
+}
+
++ (SCNScene *)sceneByNSURL:(NSURL*)url {
+    SCNScene *scene = nil;
+    
+    if (![GR isGoodGLTFByName:url.path.UTF8String]) {
+        return [SceneGenerator errorScene];
+    }
+    
+    id<GLTFBufferAllocator> bufferAllocator = [[GLTFDefaultBufferAllocator alloc] init];
+    GLTFAsset *asset = [[GLTFAsset alloc] initWithURL:url bufferAllocator:bufferAllocator];
+    
+    if (asset == nil || ![GLTFErrorCheckerByScenes isGoodGLTFByScenes:asset.scenes]) {
+        return [SceneGenerator errorScene];
+    }
+    
+    GLTFSCNAsset *scnAsset = [SCNScene assetFromGLTFAsset:asset options:@{}];
+    scene = scnAsset.defaultScene;
+    [SceneGenerator setAnimationsToScene:scene scnAsset:scnAsset];
+    
+    return scene;
+}
+
++ (CFDataRef) archivedScene: (SCNScene *) scene {
+    return (__bridge CFDataRef)[NSKeyedArchiver archivedDataWithRootObject:scene requiringSecureCoding:NO error:nil];
+}
+
++ (SCNScene *) errorScene {
+#if DEBUG
+    NSLog(@"Error Scene");
+#endif
+    NSBundle *bundle = [NSBundle bundleWithIdentifier:@"com.ITS.glTF-qucklook"];
+    
+    NSURL *urlFile =  [bundle URLForResource:@"ErrorSymbol" withExtension:@"scn"];
+    
+    return [SCNScene sceneWithURL:urlFile options:nil error:nil];
+}
+
++ (void) setAnimationsToScene: (SCNScene*) scene scnAsset:(GLTFSCNAsset *)scnAsset {
+    NSDictionary *animations = scnAsset.animations;
+    
+    if (animations.count != 0) {
+        NSString *name = scnAsset.animations.allKeys.firstObject;
+#if DEBUG
+        NSLog(@"Animation name: %@", name);
+#endif
+        
+        [animations[name] enumerateObjectsUsingBlock:^(GLTFSCNAnimationTargetPair *pair, NSUInteger index, BOOL *stop) {
+            pair.animation.usesSceneTimeBase = NO;
+            [pair.target addAnimation:pair.animation forKey:nil];
+        }];
+    }
+}
+
+
+
+@end
